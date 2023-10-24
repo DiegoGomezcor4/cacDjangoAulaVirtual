9b712ea55b6d1e5ac7071d7252c1b787b1c8e549
Diego Gomez <diegocor4@gmail.com>
2023-10-24 17:30:41 -0300

    querys

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 
 # Create your models here.
 
@@ -8,13 +9,21 @@ class Persona(models.Model):
     email = models.EmailField(max_length=150, verbose_name='Email')
     dni = models.IntegerField(verbose_name='DNI')
     
+    def clean_dni(self):
+        if not (0 < self.cleaneed_data['dni'] <= 99999999):
+            raise ValidationError('el dni debe ser un numero positivo de 8 digitos')
+        return self.cleaneed_data['dni']
+        
+    class Meta:
+        abstract = True
+    
 
-class Estudiante(models.Model):
-    nombre = models.CharField(max_length=30, verbose_name='Nombre')
-    apellido = models.CharField(max_length=30, verbose_name='Apellido')
-    email = models.EmailField(max_length=150, verbose_name='Email')
-    dni = models.IntegerField(verbose_name='DNI')
-    legajo = models.IntegerField(verbose_name='Legajo')
+class Estudiante(Persona):
+    legajo = models.CharField(max_length=100, verbose_name='Legajo')
+    
+    
+class Docente(Persona):
+    cuit = models.CharField(max_length=100, verbose_name='cuit')
     
 
 class Categoria(models.Model):
