3591286af6f8a4ef2062275d9cf2f2cfa234d477
Diego Gomez <diegocor4@gmail.com>
2023-10-24 02:56:42 -0300

    models2

--- core/models.py
+++ core/models.py
@@ -8,3 +8,23 @@ class Persona(models.Model):
     email = models.EmailField(max_length=150, verbose_name='Email')
     dni = models.IntegerField(verbose_name='DNI')
     
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30, verbose_name='Nombre')
+    apellido = models.CharField(max_length=30, verbose_name='Apellido')
+    email = models.EmailField(max_length=150, verbose_name='Email')
+    dni = models.IntegerField(verbose_name='DNI')
+    legajo = models.IntegerField(verbose_name='Legajo')
+    
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name='Nombre')
+    
+class Cursos(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name='Nombre')
+    descripcion = models.CharField(max_length=150, verbose_name='Descripcion del curso')
+    fecha_inicio = models.DateField(verbose_name='Fecha de inicio')
+    cantidad_clases = models.IntegerField(verbose_name='Cantidad de clases')
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    estudiantes = models.ManyToManyField(Estudiante)
+    
