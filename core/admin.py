2b122f225a5f12dbf17a087f208dd7bb09f9b03a
Diego Gomez <diegocor4@gmail.com>
2023-11-15 20:04:47 -0300

    admin

--- core/admin.py
+++ core/admin.py
@@ -11,20 +11,19 @@ class EstudianteAdmin(admin.ModelAdmin): # modifica la visualizacion en el admin
     list_display_links = ['legajo']
     search_fields = ['apellido']
 
-
+@admin.register(Cursos)
 class CursoAdmin(admin.ModelAdmin):
     list_display= ('nombre', 'fecha_inicio')
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == 'estudiantes':
-            kwargs['queryset'] = Estudiante.objects.filter(legajo__startswith='2').order_by('apellido')
+            kwargs['queryset'] = Estudiante.objects.filter().order_by('apellido')
 
         return super().formfield_for_manytomany(db_field, request, **kwargs)
 
 # Register your models here.
 admin.site.register(Estudiante, EstudianteAdmin)
 admin.site.register(Docente)
-admin.site.register(Cursos)
 admin.site.register(Categoria)
 admin.site.register(Inscripcion)
 
