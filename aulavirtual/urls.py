c4aaaae66f66e4a50656d4f9b5a0424456288dd9
Diego Gomez <diegocor4@gmail.com>
2023-11-19 11:24:16 -0300

    login logout

--- aulavirtual/urls.py
+++ aulavirtual/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('admin/',sitio_admin.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    path('acounts/login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('acounts/logot/', auth_views.LogoutView.as_view(template_name='core/logout.html'), name='logout'),
+    
+    
     path('alumnos/listado', views.alumnos_listado, name='alumnos_listado'),
     path('alumnos/detalle/<str:nombre_alumno>', views.alumnos_detalle, name='alumnos_detalle'),
     re_path(r'alumnos/historico/(?P<year>[0-9]{4})/$', views.alumnos_historico, name='alumnos_historico'),
